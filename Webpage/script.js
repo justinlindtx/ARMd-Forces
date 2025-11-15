e7888d2cef74c95cec4c6914329f337ff7d52640
justinlindtx <justinlindtx@gmail.com>
2025-11-15 01:20:06 -0600

    Extensive webpage formatting

--- Webpage/script.js
+++ Webpage/script.js
@@ -7,6 +7,15 @@ function main() {
 	selectElement.value = selectedMode;
 	let event = new Event('change');
     selectElement.dispatchEvent(event);
+
+	var snapshotBtn = document.getElementById("snapshot-btn");
+	var pauseBtn = document.getElementById("pause-btn");
+	var gripBtn = document.getElementById("grip-btn");
+	var undoBtn = document.getElementById("undo-btn");
+	snapshotBtn.addEventListener("click", takeSnapshot);
+	pauseBtn.addEventListener("click", addPause);
+	gripBtn.addEventListener("click", changeGrip);
+	undoBtn.addEventListener("click", undoAction);
 }
 
 function modeChange() {
@@ -14,8 +23,37 @@ function modeChange() {
 	localStorage.setItem("mode", selectedMode);
 	var modes = document.getElementsByClassName("mode");
 	var selMode = document.getElementById(this.value);
-	for (let i = 0; i < modes.length; i++) {
+	for (let i = 0; i < modes.length; i++) { // hide all modes
 		modes[i].style.display = "none";
 	}
-	selMode.style.display = "block";
+	selMode.style.display = "block"; // show selected mode
+
+	var controlPanel = document.getElementById("control-panel");
+	if(selectedMode == "manual"){
+		var placeholder = document.getElementById("manual-control-placeholder");
+		placeholder.appendChild(controlPanel);
+	}
+	else if(selectedMode == "create"){
+		var placeholder = document.getElementById("control-placeholder");
+		placeholder.appendChild(controlPanel);
+	}
+}
+
+function undoAction() {
+	var list = document.getElementById("list");
+	if(list.lastChild){
+		list.removeChild(list.lastChild);
+	}
+}
+
+function addPause() {
+
+}
+
+function takeSnapshot() {
+
+}
+
+function changeGrip() {
+	
 }
